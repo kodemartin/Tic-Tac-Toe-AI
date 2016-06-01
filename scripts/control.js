ea18c5cca98e99c8947cf4dd9d2ae466bf7b468c
kodemartin <kodemartin@protonmail.ch>
2016-06-01 18:31:05 +0300

    Updating index.html, ui.css and control.js

--- scripts/control.js
+++ scripts/control.js
@@ -1,63 +1,50 @@
-/*
- * object to contain all items accessable to all control functions
+/**
+ * @author kodemartin
  */
-var globals = {};
 
-/*
- * choosing difficulty level (onclick span.level) behavior and control
- * when a level is clicked, it becomes highlighted and the "ai.level" variable
- * is set to the chosen level
- */
-$(".level").each(function() {
+
+//$( document ).ready( function () { 
+
+var computer = new AI("novice");
+var game0 = new Game(computer);
+computer.plays(game0);
+ 
+    //populate the board array
+
+$("td").each(function(index){
+    $(this).addClass("cell");
     var $this = $(this);
-    $this.click(function() {
-        $('.selected').toggleClass('not-selected');
-        $('.selected').toggleClass('selected');
-        $this.toggleClass('not-selected');
-        $this.toggleClass('selected');
+    $(this).on("click", function() {
+        if(game0.status === "running" && game0.currentState.turn === human && !$this.hasClass('occupied')) {
 
-        ai.level = $this.attr("id");
-    });
+            var next = new State(game0.currentState);
+            next.board[index] = human;
+
+            ui.insertAt(index, human);
+
+            next.advanceTurn();
+
+            game0.advanceTo(next);
+
+
+        }});
 });
+var board = $(".cell");
 
-/*
- * start game (onclick div.start) behavior and control
- * when start is clicked and a level is chosen, the game status changes to "running"
- * and UI view to swicthed to indicate that it's human's trun to play
- */
-$(".start").click(function() {
-    var selectedDiffeculty = $('.selected').attr("id");
-    if(typeof selectedDiffeculty !== "undefined") {
-        var aiPlayer = new AI(selectedDiffeculty);
-        globals.game = new Game(aiPlayer);
-
-        aiPlayer.plays(globals.game);
-
-        globals.game.start();
+$("#play").on("click", function(){
+    if (human === undefined) {
+        $("#whoplays").text("Please select side!");
+    } else if (game0.status === "beginning") {
+        game0.start();
+    } else if (game0.status === "ended") {
+        rematch();
+        game0 = new Game(computer);
+        computer.plays(game0);
+        game0.start()
+    } else {
+        $("#whoplays").text("Game is already running!");
     }
 });
 
-/*
- * click on cell (onclick div.cell) behavior and control
- * if an empty cell is clicked when the game is running and its the human player's trun
- * get the indecies of the clickd cell, craete the next game state, upadet the UI, and
- * advance the game to the new created state
- */
- $(".cell").each(function() {
-     var $this = $(this);
-     $this.click(function() {
-         if(globals.game.status === "running" && globals.game.currentState.turn === "X" && !$this.hasClass('occupied')) {
-             var indx = parseInt($this.data("indx"));
 
-             var next = new State(globals.game.currentState);
-             next.board[indx] = "X";
-
-             ui.insertAt(indx, "X");
-
-             next.advanceTurn();
-
-             globals.game.advanceTo(next);
-
-         }
-     })
- });
+//});
