1f59380a2b8141b497834b9a662b1062f856bb0e
kodemartin <kodemartin@protonmail.ch>
2017-04-21 22:17:18 +0300

    Correct ai scripts

--- scripts/side.js
+++ scripts/side.js
@@ -1,14 +1,6 @@
-/*
- * This script lets the user choose 
- * which side to play (X or O). It 
- * saves the choice on global variables, 
- * subsequently called on the main algorithm
- *
- * For further information on the html elements (e.g #whoplays)
- * see main.html and layout.css
- */
+//$(document).ready ( function(){
+
 $("#whoplays").html("Choose side and press play when ready. X starts first.");
-// Define the global variables
 var human;
 var robot;
 
@@ -20,10 +12,6 @@ $(".player").each(function(){
         $("#whoplays").html("You will play with " + human + "<br>Press " + $("#play").html()+ " when ready.");
     })
 });
-
-/* Function to clear the board on rematch
- * request.
- */
 function rematch() {
     $("td").each(function(index){
         $(this).removeClass("occupied");
@@ -31,3 +19,4 @@ function rematch() {
     })
 }
 
+//})
